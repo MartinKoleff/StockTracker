ac4ad21f88359600d056c2f6aae36fb22f477dd7
MartinKoleff <martin_koleff@abv.bg>
2023-09-16 16:51:18 +0300

    Setting up MultiDex application class.

--- app/src/main/java/com/koleff/resumeproject/KoleffApp.java
+++ app/src/main/java/com/koleff/resumeproject/KoleffApp.java
@@ -1,6 +1,24 @@
 package com.koleff.resumeproject;
 
 import android.app.Application;
+import android.content.Context;
+import android.util.Log;
 
-public class KoleffApp extends Application {
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.multidex.MultiDex;
+import androidx.multidex.MultiDexApplication;
+
+public class KoleffApp extends MultiDexApplication { //implements DefaultLifecycleObserver
+    /**
+     * Called first
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
+    }
 }
