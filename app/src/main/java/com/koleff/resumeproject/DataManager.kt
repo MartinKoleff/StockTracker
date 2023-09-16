8328a95872fd96a14243b99d72f882ad953fb123
MartinKoleff <martin_koleff@abv.bg>
2023-09-16 16:48:45 +0300

    Added DataManager to store globally accessed fields.

---
+++ app/src/main/java/com/koleff/resumeproject/DataManager.kt
@@ -0,0 +1,8 @@
+package com.koleff.resumeproject
+
+import android.content.SharedPreferences
+
+object DataManager {
+
+//    val sharedPreferences: SharedPreferences by lazy { KoleffApp.getSharedPreferences() }
+}
