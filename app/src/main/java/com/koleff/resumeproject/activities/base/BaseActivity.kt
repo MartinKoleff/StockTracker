a30615b5776984de08b15475b67e0a5217d6e7a6
MartinKoleff <martin_koleff@abv.bg>
2023-09-17 19:10:22 +0300

    Refactored binding to be explicitly not nullt.

--- app/src/main/java/com/koleff/resumeproject/activities/base/BaseActivity.kt
+++ app/src/main/java/com/koleff/resumeproject/activities/base/BaseActivity.kt
@@ -1,8 +1,10 @@
 package com.koleff.resumeproject.activities.base
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.WindowManager
 import androidx.viewbinding.ViewBinding
 
 abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
@@ -10,14 +12,23 @@ abstract class BaseActivity<VB : ViewBinding> : AppCompatActivity() {
     private var _binding: VB? = null
     abstract val bindingInflater: (LayoutInflater) -> VB
 
-    private val binding: VB?
-        get() = _binding
+    val binding: VB
+        get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         _binding = bindingInflater.invoke(layoutInflater)
         setContentView(binding!!.root)
+
+//        val window = window
+//        if (Build.VERSION.SDK_INT >= 21) {
+//            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+//            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+////            window.statusBarColor = Colors.StatusBar[Colors.theme]
+//        }
+//
+//        window.decorView.setBackgroundColor(-0x1)
 
         setup()
     }
