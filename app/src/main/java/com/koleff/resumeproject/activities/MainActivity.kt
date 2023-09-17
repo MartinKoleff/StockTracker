31a0e0433e44c45339e4d98151730fcd35c6705b
MartinKoleff <martin_koleff@abv.bg>
2023-09-17 18:35:03 +0300

    Wiring navigation drawer button.

--- app/src/main/java/com/koleff/resumeproject/activities/MainActivity.kt
+++ app/src/main/java/com/koleff/resumeproject/activities/MainActivity.kt
@@ -3,15 +3,22 @@ package com.koleff.resumeproject.activities
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import com.koleff.resumeproject.activities.base.BaseActivity
 import com.koleff.resumeproject.databinding.ActivityMainBinding
 
-class MainActivity: BaseActivity<ActivityMainBinding>() {
+class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override val bindingInflater: (LayoutInflater) -> ActivityMainBinding =
         ActivityMainBinding::inflate
-    override fun setup() {
 
+    override fun setup() = with(binding) {
+        containerMain.toolbar.ivNavigationDrawer.setOnClickListener {
+            if (dlNavigation.isDrawerOpen(navigationDrawer)) {
+                dlNavigation.closeDrawer(GravityCompat.START)
+            } else {
+                dlNavigation.openDrawer(GravityCompat.START)
+            }
+        }
     }
-
 }
