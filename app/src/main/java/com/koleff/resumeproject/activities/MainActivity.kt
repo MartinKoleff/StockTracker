4cf32266a63b2693f18a41d016b9df9d254f0ca2
MartinKoleff <martin_koleff@abv.bg>
2023-09-17 20:33:50 +0300

    Wiring settings adapter with the list view.

--- app/src/main/java/com/koleff/resumeproject/activities/MainActivity.kt
+++ app/src/main/java/com/koleff/resumeproject/activities/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.koleff.resumeproject.activities
 
-import android.os.Bundle
 import android.view.LayoutInflater
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.ListView
 import androidx.core.view.GravityCompat
+import com.koleff.resumeproject.R
 import com.koleff.resumeproject.activities.base.BaseActivity
+import com.koleff.resumeproject.adapters.AdapterNavigationSettings
+import com.koleff.resumeproject.adapters.SettingItem
 import com.koleff.resumeproject.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
@@ -13,6 +15,8 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         ActivityMainBinding::inflate
 
     override fun setup() = with(binding) {
+
+        //Navigation drawer
         containerMain.toolbar.ivNavigationDrawer.setOnClickListener {
             if (dlNavigation.isDrawerOpen(navigationDrawer)) {
                 dlNavigation.closeDrawer(GravityCompat.START)
@@ -20,5 +24,18 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
                 dlNavigation.openDrawer(GravityCompat.START)
             }
         }
+
+        //Navigation settings list
+        val listSettings = findViewById<ListView>(R.id.list_settings)
+
+        var navigationSettings = mutableListOf<SettingItem>()
+        fillNavigationSettings(navigationSettings)
+
+        val adapterSettings = AdapterNavigationSettings(this@MainActivity, navigationSettings)
+        listSettings.adapter = adapterSettings
+    }
+
+    private fun fillNavigationSettings(list: MutableList<SettingItem>) {
+        list.add(SettingItem(SettingItem.id_language, resources.getString(R.string.text_language), R.drawable.ic_language))
     }
 }
