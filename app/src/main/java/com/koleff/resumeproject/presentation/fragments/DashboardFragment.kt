df2abc15d6d6dfbe323e9b731308622154a29cd3
MartinKoleff <martin_koleff@abv.bg>
2023-09-23 23:21:49 +0300

    Added Dashboard fragment.
    Added filter custom layout. Custom filters may be:
    - ascending stock price
    - descending stock price
    - based on stock exchange place

---
+++ app/src/main/java/com/koleff/resumeproject/presentation/fragments/DashboardFragment.kt
@@ -0,0 +1,16 @@
+package com.koleff.resumeproject.presentation.fragments
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.koleff.resumeproject.databinding.FragmentDashboardBinding
+import com.koleff.resumeproject.presentation.fragments.base.BaseFragment
+
+class DashboardFragment: BaseFragment<FragmentDashboardBinding>() {
+
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentDashboardBinding =
+        FragmentDashboardBinding::inflate
+
+    override fun setup() {
+        TODO("Not yet implemented")
+    }
+}
