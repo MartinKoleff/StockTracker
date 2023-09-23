909dea72ba8c00c8fa1f54125e89b19acf3b01ee
MartinKoleff <martin_koleff@abv.bg>
2023-09-23 15:07:06 +0300

    Using WeakReference to prevent memory leak. Added custom configurations for different errors.

--- app/src/main/java/com/koleff/resumeproject/common/ErrorHandler.kt
+++ app/src/main/java/com/koleff/resumeproject/common/ErrorHandler.kt
@@ -1,30 +1,44 @@
 package com.koleff.resumeproject.common
 
+import android.app.Activity
 import com.koleff.resumeproject.KoleffApp
 import com.koleff.resumeproject.R
 import com.koleff.resumeproject.domain.wrappers.networkWrappers.KoleffError
+import java.lang.ref.WeakReference
 
 object ErrorHandler {
 
-    private val activeActivity = KoleffApp.getActiveActivity()
+    private val activeActivity: WeakReference<Activity> by lazy {WeakReference(KoleffApp.getActiveActivity())}
 
     fun showError(error: KoleffError? = null, errorMessage: String? = null) {
-        val neutralButtonText = activeActivity.getString(R.string.text_ok)
-        val positiveButtonText = activeActivity.getString(R.string.text_yes)
-        val negativeButtonText = activeActivity.getString(R.string.text_cancel)
+        activeActivity.get() != null || return
+
+        val neutralButtonText = activeActivity.get()!!.getString(R.string.text_ok)
+        val positiveButtonText = activeActivity.get()!!.getString(R.string.text_yes)
+        val negativeButtonText = activeActivity.get()!!.getString(R.string.text_cancel)
 
         val dialogError: KoleffError = error ?: KoleffError.GENERIC
-        val dialogMessage: String = errorMessage ?: activeActivity.getString(dialogError.errorMessage)
+        val dialogMessage: String =
+            errorMessage ?: activeActivity.get()!!.getString(dialogError.errorMessage)
 
-//        val dialogMessage = errorMessage
-//            ?: activeActivity.getString(error?.errorMessage ?: dialogError.errorMessage)
-
-        ErrorDialog.Builder()
+        val errorDialog = ErrorDialog.Builder()
             .setMessage(dialogMessage)
             .setError(error)
-            .setNeutralButton(neutralButtonText)
-            .setPositiveButton(positiveButtonText)
-            .setNegativeButton(negativeButtonText)
-            .show(activeActivity)
+
+        when (dialogError) {
+            KoleffError.GENERIC -> {
+                errorDialog.setNeutralButton(neutralButtonText)
+                    .setPositiveButton(positiveButtonText)
+                    .setNegativeButton(negativeButtonText)
+            }
+
+            else -> {
+                errorDialog
+                    .setPositiveButton(positiveButtonText)
+                    .setNegativeButton(negativeButtonText)
+            }
+        }
+
+        errorDialog.show(activeActivity.get()!!)
     }
 }
