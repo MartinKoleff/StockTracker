a7f01dd272e3170a8d1ba79596e927a9982015a6
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-09-25 18:03:38 +0300

    Added caching for API response objects.

--- app/src/main/java/com/koleff/resumeproject/common/managers/DataManager.kt
+++ app/src/main/java/com/koleff/resumeproject/common/managers/DataManager.kt
@@ -1,15 +1,32 @@
 package com.koleff.resumeproject.common.managers
 
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.koleff.resumeproject.KoleffApp
 import com.koleff.resumeproject.common.Constants
 import com.koleff.resumeproject.domain.wrappers.StockData
 import com.koleff.resumeproject.domain.wrappers.TickerData
 
 object DataManager {
-    val apiUrl = if(!Constants.useFakeApi) Constants.BASE_URL else Constants.FAKE_BASE_URL
+    //Constants
+    private const val STOCKS_CACHE = "STOCKS_CACHE"
 
-//    val sharedPreferences: SharedPreferences by lazy { KoleffApp.getSharedPreferences() }
+    private val sharedPreferences: SharedPreferences by lazy { KoleffApp.getSharedPreferences() }
+    private val gson = Gson()
+    var stocks: List<StockData> =
+        Gson().fromJson(sharedPreferences.getString(STOCKS_CACHE, "[]"),
+        object : TypeToken<List<StockData>>() {}.type
+    )
+        set(value){
+            field != value || return
 
-    lateinit var stocks: List<StockData>
+            field = value
+            sharedPreferences.edit().putString(STOCKS_CACHE, gson.toJson(value)).apply()
+            //Send broadcast to notify UI...
+        }
     lateinit var tickers: List<TickerData>
     lateinit var selectedTicker: TickerData
+
+    val apiUrl = if(!Constants.useFakeApi) Constants.BASE_URL else Constants.FAKE_BASE_URL
 }
