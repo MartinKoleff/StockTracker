04f6d2cae9a34da17c827c42212c057442829a49
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-09-26 16:02:12 +0300

    Implemented stateFlow hot flow instead of cold flow.
    Cold flows are created on-demand and emit data when they’re being observed. Hot flows are always active and can emit data regardless of whether or not they’re being observed.

--- app/src/main/java/com/koleff/resumeproject/presentation/viewModels/StockMarketViewModel.kt
+++ app/src/main/java/com/koleff/resumeproject/presentation/viewModels/StockMarketViewModel.kt
@@ -1,6 +1,10 @@
 package com.koleff.resumeproject.presentation.viewModels
 
 import android.util.Log
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.koleff.resumeproject.KoleffApp
@@ -14,10 +18,14 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -27,14 +35,23 @@ class StockMarketViewModel @Inject constructor(
     private val dispatcher: CoroutineDispatcher = Dispatchers.Main
 ) : ViewModel() {
 
-    private var _state: Flow<List<StockData>> = flowOf<List<StockData>>()
-    val state: Flow<List<StockData>> = _state
+//    private var _state: MutableState<List<StockData>> = mutableStateOf(emptyList())
+//    val state: MutableState<List<StockData>>
+
+    private val _state: MutableStateFlow<List<StockData>> =
+        MutableStateFlow(emptyList())//flowOf(emptyList())
+    val state: StateFlow<List<StockData>>
+        get() = _state
+
+    override fun onCleared() {
+        super.onCleared()
+    }
 
     init {
         getStockData("AAPL", "2023-09-10", "2023-09-18")
     }
 
-    private fun getStockData(stockTag: String, dateFrom: String, dateTo: String) {
+    fun getStockData(stockTag: String, dateFrom: String, dateTo: String) {
         viewModelScope.launch(dispatcher) {
             stockMarketRepository.getStockData(stockTag, dateFrom, dateTo).onEach { apiResult ->
                 when (apiResult) {
@@ -52,9 +69,17 @@ class StockMarketViewModel @Inject constructor(
                     is ResultWrapper.Success -> {
                         DataManager.stocks =
                             apiResult.data.stockData.map(StockDataDto::toStockData).also {
-                                Log.d(KoleffApp.TAG_LOG, it.toString())
+//                                Log.d(KoleffApp.TAG_LOG, it.toString())
 
-                                _state = flowOf(it)
+//                                _state = mutableStateOf(it)
+
+//                                _state = flowOf(it).stateIn(
+//                                    initialValue = it,
+//                                    scope = viewModelScope,
+//                                    started = SharingStarted.WhileSubscribed(5000)
+//                                )
+
+                                _state.value = it
                             }
                     }
                 }
