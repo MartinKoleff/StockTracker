44fa3890766023b40f906c2298fe943482670fc2
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-11-08 18:22:15 +0200

    Wired StockData with StockDto.

--- app/src/main/java/com/koleff/resumeproject/domain/wrappers/StockData.kt
+++ app/src/main/java/com/koleff/resumeproject/domain/wrappers/StockData.kt
@@ -1,23 +1,30 @@
 package com.koleff.resumeproject.domain.wrappers
 
+import com.koleff.resumeproject.data.dto.EndOfDayDto
+import com.koleff.resumeproject.data.dto.IntraDayDto
+import com.koleff.resumeproject.data.dto.StockDto
+import java.util.Date
+
 //Used for presentation layer creation.
-data class StockData (val stockDataDto: StockDataDto){
-    val date = stockDataDto.date
-    val stockTag = stockDataDto.stockTag
-    val exchangeTag = stockDataDto.exchangeTag
-    val openPrice = stockDataDto.openPrice
-    val closePrice = stockDataDto.closePrice
-    val highPrice = stockDataDto.highPrice
-    val lowPrice= stockDataDto.lowPrice
-    val volume = stockDataDto.volume
-    val adjustOpenPrice = stockDataDto.adjustOpenPrice
-    val adjustClosePrice = stockDataDto.adjustClosePrice
-    val adjustHighPrice = stockDataDto.adjustHighPrice
-    val adjustLowPrice = stockDataDto.adjustLowPrice
-    val adjustVolume = stockDataDto.adjustVolume
-    val splitFactor = stockDataDto.splitFactor
-    val dividend = stockDataDto.dividend
-    val isRising: Boolean = stockDataDto.openPrice - stockDataDto.closePrice > 0
-    val dayDifference: Double = stockDataDto.openPrice - stockDataDto.closePrice
-    val changePercent: Double = stockDataDto.openPrice / dayDifference * 100
+data class StockData (val stockDto: StockDto){
+    private val currentDateIntraDay: IntraDayDto? = stockDto.intraDayList.find { it.date == Date() }
+    private val currentDateEndOfDay: EndOfDayDto? = stockDto.endOfDayList.find { it.date == Date() }
+    val stockTag = stockDto.tag
+    val exchangeTag = stockDto.stockExchange.name
+    val openPrice = currentDateIntraDay?.open ?: 0.0
+    val closePrice = currentDateIntraDay?.close ?: 0.0
+    val highPrice= currentDateIntraDay?.high ?: 0.0
+    val lowPrice = currentDateIntraDay?.low ?: 0.0
+    val volume = currentDateIntraDay?.volume ?: 0.0
+    val adjustOpenPrice = currentDateIntraDay
+    val adjustClosePrice = currentDateEndOfDay?.adjustClose ?: 0.0
+    val adjustHighPrice = currentDateEndOfDay?.adjustHigh ?: 0.0
+    val adjustLowPrice = currentDateEndOfDay?.adjustLow ?: 0.0
+    val adjustVolume = currentDateEndOfDay?.adjustVolume ?: 0.0
+    val splitFactor = currentDateEndOfDay?.splitFactor ?: 0.0
+    val dividend = currentDateEndOfDay?.dividend ?: 0.0
+    val isRising: Boolean = openPrice - closePrice > 0
+    val dayDifference = openPrice - closePrice
+    val changePercent = openPrice / dayDifference * 100
+    val date = Date()
 }
