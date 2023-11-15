27546972ee2ccdff34e4286f120d2fa04cff37ef
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-11-15 23:40:17 +0200

    Added some constants.

--- app/src/main/java/com/koleff/resumeproject/common/Constants.kt
+++ app/src/main/java/com/koleff/resumeproject/common/Constants.kt
@@ -1,18 +1,21 @@
 package com.koleff.resumeproject.common
 
 object Constants {
-    const val API_KEY = "38759d046e883748e2947cc8a1bd4665"
+    const val API_KEY = "cf3ee1fcf45260ed96bf958093517b6d"
 
     const val SCHEME_LOCAL = "http"
     const val SCHEME = "https"
+    const val PORT = 8081
 
     const val BASE_URL = "api.marketstack.com"
     const val FAKE_BASE_URL = "demo6160472.mockable.io"
 
-    const val BASE_LOCAL_URL = "https://localhost"
+    const val BASE_LOCAL_URL = "localhost"
+    const val BASE_LOCAL_URL_FULL = "$SCHEME_LOCAL://$BASE_LOCAL_URL"
+    const val BASE_LOCAL_URL_FULL_PORT = "$SCHEME_LOCAL://$BASE_LOCAL_URL:$PORT/"
 
-    const val BASE_URL_FULL = "$SCHEME_LOCAL://$BASE_URL/" //v1/eod?access_key=$API_KEY&symbols=AAPL
+    const val BASE_URL_FULL = "$SCHEME_LOCAL://$BASE_URL/"
     const val FAKE_BASE_URL_FULL = "$SCHEME_LOCAL://$FAKE_BASE_URL/"
 
-    const val useFakeApi = true
+    const val useFakeApi = false
 }
