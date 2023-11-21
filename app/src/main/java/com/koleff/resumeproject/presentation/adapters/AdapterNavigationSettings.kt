ba500c7f1d3332e16d6a14c9d9e6a1cef2e2941c
MartinKoleff <52703399+MartinKoleff@users.noreply.github.com>
2023-11-21 12:30:57 +0200

    Imports cleanup in AdapterNavigationSettings.

--- app/src/main/java/com/koleff/resumeproject/presentation/adapters/AdapterNavigationSettings.kt
+++ app/src/main/java/com/koleff/resumeproject/presentation/adapters/AdapterNavigationSettings.kt
@@ -1,9 +1,7 @@
-package com.koleff.resumeproject.presentation.activities.adapters
+package com.koleff.resumeproject.presentation.adapters
 
 import android.app.Activity
 import android.content.Context
-import android.graphics.Typeface
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,7 +9,6 @@ import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
-import com.koleff.resumeproject.KoleffApp
 import com.koleff.resumeproject.R
 import com.koleff.resumeproject.common.types.KoleffFonts
 
